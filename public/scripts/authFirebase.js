be4b6f3639f5973162608fc446c6452482039e4f
JustGinger888 <ethanmoffatt888@icloud.com>
2020-06-23 22:01:20 +0100

    create

--- public/scripts/authFirebase.js
+++ public/scripts/authFirebase.js
@@ -1,19 +1,51 @@
+// Listen for auth state change
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      console.log('User logged in: ', user);
+      db.collection("guides").get().then(querySnapshot => {
+        setupGuides(querySnapshot.docs)
+      });
+      setupUI(user);
+    } else {
+      setupGuides([]);
+      setupUI();
+    }
+});
+
+// Create New Guide
+const createForm = document.querySelector('#create-form');
+
+createForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  db.collection("guides").add({
+    title: createForm['title'].value,
+    content: createForm['content'].value,
+  }).then(() => {
+    const modal =document.querySelector('#modal-create');
+    M.Modal.getInstance(modal).close();
+    createForm.reset();
+  }).catch(error => {
+    console.log('Insufficient Permissions');
+  });
+});
+
 // Sign Up
 const signupForm = document.querySelector('#signup-form');
 
 signupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    // get user info
-    const email = signupForm['signup-email'].value;
-    const password = signupForm['signup-password'].value;
-    
-    //Sign Up the User
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-            console.log(cred)
-            const modal =document.querySelector('#modal-signup');
-            M.Modal.getInstance(modal).close();
-            signupForm.reset();
-        });
+  e.preventDefault();
+
+  // get user info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+  
+  //Sign Up the User
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+          const modal =document.querySelector('#modal-signup');
+          M.Modal.getInstance(modal).close();
+          signupForm.reset();
+      });
 });
 
 //Logout
@@ -23,7 +55,23 @@ logout.addEventListener('click', (e) => {
     e.preventDefault();
     
     //Sign out the User
-    auth.signOut().then(() =>{
-      console.log('User signed out');
-    });
+    auth.signOut()
+});
+
+// Login
+const loginForm = document.querySelector('#login-form');
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+    
+    //Sign Up the User
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+            const modal =document.querySelector('#modal-login');
+            M.Modal.getInstance(modal).close();
+            loginForm.reset();
+        });
 });
--- public/scripts/authPage.js
+++ public/scripts/authPage.js
@@ -1,3 +1,42 @@
+const guideList = document.querySelector('.guides')
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+
+//Setup UI
+const setupUI = (user) => {
+  if (user) {
+    loggedInLinks.forEach(item => item.style.display = 'block');
+    loggedOutLinks.forEach(item => item.style.display = 'none');
+  } else {
+    loggedInLinks.forEach(item => item.style.display = 'none');
+    loggedOutLinks.forEach(item => item.style.display = 'block');
+  }
+}
+
+//Setup Grid
+const setupGuides = (data) => {
+  if (data.length) {
+    let html = '';
+    data.forEach(doc => {
+      console.log(doc.data)
+      const guide = doc.data();
+      const li = `
+      <li>
+          <div class="collapsible-header grey lighten-4">${guide.title}</div>
+          <div class="collapsible-body white">${guide.content}</div>
+      </li>
+      `;
+      html += li;
+    });
+
+    guideList.innerHTML = html;
+
+  } else {
+    guideList.innerHTML = '<h5 class="center-align">Sign in to view Docs<h5>';
+  }
+}
+
+// Modal Setup
 document.addEventListener('DOMContentLoaded', function() {
 
     var modals = document.querySelectorAll('.modal');
@@ -7,3 +46,4 @@ document.addEventListener('DOMContentLoaded', function() {
     M.Collapsible.init(items);
   
   });
+
