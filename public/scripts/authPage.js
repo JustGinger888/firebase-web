eb36a91d9437b9471b4199b0a25e8653def12d9d
JustGinger888 <ethanmoffatt888@icloud.com>
2020-06-23 07:02:39 +0100

    signUp

---
+++ public/scripts/authPage.js
@@ -0,0 +1,17 @@
+// Sign Up
+const signupForm = document.querySelector('#signup-form');
+
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+    
+    //Sign Up the User
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+            console.log(cred)
+            const modal =document.querySelector('#modal-signup');
+            M.Modal.getInstance(modal).close();
+            signupForm.reset();
+        });
+});
