aeedefd0855bd2465c2104bc0672c906b27e828a
JustGinger888 <ethanmoffatt888@icloud.com>
2020-06-23 07:15:48 +0100

    sign out

--- public/scripts/authFirebase.js
+++ public/scripts/authFirebase.js
@@ -1,9 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
+// Sign Up
+const signupForm = document.querySelector('#signup-form');
 
-    var modals = document.querySelectorAll('.modal');
-    M.Modal.init(modals);
-  
-    var items = document.querySelectorAll('.collapsible');
-    M.Collapsible.init(items);
-  
-  });
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+    
+    //Sign Up the User
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+            console.log(cred)
+            const modal =document.querySelector('#modal-signup');
+            M.Modal.getInstance(modal).close();
+            signupForm.reset();
+        });
+});
+
+//Logout
+const logout = document.querySelector('#logout');
+
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    //Sign out the User
+    auth.signOut().then(() =>{
+      console.log('User signed out');
+    });
+});
--- public/scripts/authPage.js
+++ public/scripts/authPage.js
@@ -1,17 +1,9 @@
-// Sign Up
-const signupForm = document.querySelector('#signup-form');
+document.addEventListener('DOMContentLoaded', function() {
 
-signupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    // get user info
-    const email = signupForm['signup-email'].value;
-    const password = signupForm['signup-password'].value;
-    
-    //Sign Up the User
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-            console.log(cred)
-            const modal =document.querySelector('#modal-signup');
-            M.Modal.getInstance(modal).close();
-            signupForm.reset();
-        });
-});
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+  
+    var items = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(items);
+  
+  });
