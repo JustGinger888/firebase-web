e2c569e65f5c7ffe55dde39467219ac0f28e8409
JustGinger888 <ethanmoffatt888@icloud.com>
2020-06-24 03:35:31 +0100

    error catch

--- public/scripts/authFirebase.js
+++ public/scripts/authFirebase.js
@@ -70,6 +70,9 @@ signupForm.addEventListener('submit', (e) => {
     const modal =document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
     signupForm.reset();
+    signupForm.querySelector('.error').innerHTML = '';
+  }).catch(err => {
+    signupForm.querySelector('.error').innerHTML = err.message;
   });
 });
 
@@ -98,5 +101,8 @@ loginForm.addEventListener('submit', (e) => {
             const modal =document.querySelector('#modal-login');
             M.Modal.getInstance(modal).close();
             loginForm.reset();
+            oginForm.querySelector('.error').innerHTML = '';
+        }).catch(err => {
+          loginForm.querySelector('.error').innerHTML = err.message;
         });
 });
